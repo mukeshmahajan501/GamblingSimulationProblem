6664bc72454838b7273e104163a7f175b7ada177
mukesh <mukeshmahajan501@gmail.com>
2020-09-10 21:40:02 +0530

    [mukesh]:added-Gambler Win Or Loose

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,5 +1,20 @@
 public class GamblingSimulation{
 	//declare constant variable
-	final int STAKE_AMOUNT = 100;
-	final int BET_AMOUNT = 1;
+	final static int STAKE_AMOUNT = 100;
+	final static int BET_AMOUNT = 1;
+	
+	public static void main(String args[]){
+	//to get random number between 0 or 1
+	int randomCheck = (int)Math.floor(Math.random()*10%2);
+	if(randomCheck == 1)
+	{
+		System.out.println("you won $1");
+	}
+	else
+	{
+		System.out.println("you loose $1");
+	}
+
+}
+	
 }
