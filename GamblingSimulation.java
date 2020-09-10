8b76e9f55572f0ba42a86904881fdf6aefe0b492
mukesh <mukeshmahajan501@gmail.com>
2020-09-10 22:49:17 +0530

    [mukesh]:added-Gambler win or lost for month and how much

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -4,10 +4,12 @@ public class GamblingSimulation {
 	final int BET = 1;
 	final int WINING_CASH = 150;
 	final int LOOSING_CASH = 50;
-	final int NO_OF_DAYS = 20;
+	final int NO_OF_DAYS = 30;
 	final int STAKE_PERCENTAGE = 50;
 	int result;
 	int dayResult = 0;
+	int win = 0;
+	int lost = 0;
 
 	public void getTotalAmountWinOrLost() {
 
@@ -27,19 +29,25 @@ public class GamblingSimulation {
 
 			if (result == WINING_CASH) {
 				dayResult = dayResult + 1;
+				win = win + STAKE_PERCENTAGE;
 				System.out.println("total Amount won in day:" + day + ": " + result);
 
 			} else {
 				dayResult = dayResult - 1;
+				lost = lost - STAKE_PERCENTAGE;
 				System.out.println("total amount lost in days:" + day + ": " + +result);
 			}
 
 		}
+
+		System.out.println();
+		System.out.println("total win: " + win);
+		System.out.println("total lost: " + Math.abs(lost));
 		int totalAmount = dayResult * STAKE_PERCENTAGE;
 
 		if (totalAmount == 0) {
 			System.out.println("---------------------------------------");
-			System.out.println("No lost No win: "+totalAmount);
+			System.out.println("Amount constant: no lost no win");
 		} else if (totalAmount > 0) {
 			System.out.println("---------------------------------------");
 
