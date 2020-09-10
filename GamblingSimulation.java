a2cc1d8961c653aad2e8d8bd2351b71b13417319
mukesh <mukeshmahajan501@gmail.com>
2020-09-10 22:18:27 +0530

    [mukesh]:added-20 days win or lost amount

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,40 +1,60 @@
 public class GamblingSimulation {
 	// declare constant variable
-	final int STAKE_AMOUNT = 100;
-	final int BET_AMOUNT = 1;
+	final int STAKE = 100;
+	final int BET = 1;
 	final int WINING_CASH = 150;
 	final int LOOSING_CASH = 50;
+	final int NO_OF_DAYS = 20;
+	final int STAKE_PERCENTAGE = 50;
+	int result;
+	int dayResult = 0;
 
-	int result = STAKE_AMOUNT;
+	public void getTotalAmountWinOrLost() {
 
-	// Gambling won or lost on 50% of stake
-	public void checkWinLoose() {
-		while (result != WINING_CASH && result != LOOSING_CASH) {
+		for (int day = 1; day <= NO_OF_DAYS; day++) {
+			result = STAKE;
+			while ((result != WINING_CASH) && (result != LOOSING_CASH)) {
+				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-			// to get random number between 0 or 1.
-			int bettingResult = (int) (Math.floor(Math.random() * 10) % 2);
-			if (bettingResult == 1) {
-				result = result + BET_AMOUNT;
+				if (randomCheck == 1) {
+					result = result + BET;
+
+				} else {
+					result = result - BET;
+				}
+
+			}
+
+			if (result == WINING_CASH) {
+				dayResult = dayResult + 1;
+				System.out.println("total Amount won in day:" + day + ": " + result);
+
 			} else {
-				result = result - BET_AMOUNT;
+				dayResult = dayResult - 1;
+				System.out.println("total amount lost in days:" + day + ": " + +result);
 			}
 
 		}
+		int totalAmount = dayResult * STAKE_PERCENTAGE;
 
-		if (result == WINING_CASH) {
-			System.out.println("you won the game");
-			System.out.println("stake: " + result);
+		if (totalAmount == 0) {
+			System.out.println("---------------------------------------");
+			System.out.println("No lost No win: "+totalAmount);
+		} else if (totalAmount > 0) {
+			System.out.println("---------------------------------------");
+
+			System.out.println("you won total amount: " + totalAmount);
 		} else {
-			System.out.println("you loose the game");
-			System.out.println("stake: " + result);
+			System.out.println("---------------------------------------");
+
+			System.out.println("you lost total amount: " + Math.abs(totalAmount));
 		}
 
 	}
 
-	public static void main(String args[]) {
-
-		GamblingSimulation gs = new GamblingSimulation();
-		gs.checkWinLoose();
+	public static void main(String[] args) {
+		GamblingSimulation obj = new GamblingSimulation();
+		obj.getTotalAmountWinOrLost();
 	}
 
 }
