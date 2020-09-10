e644a936055e09ef365b1fc6f631721864d77e8d
mukesh <mukeshmahajan501@gmail.com>
2020-09-10 21:48:33 +0530

    [mukesh]:added-win or lost on 50% stake

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,20 +1,40 @@
-public class GamblingSimulation{
-	//declare constant variable
-	final static int STAKE_AMOUNT = 100;
-	final static int BET_AMOUNT = 1;
-	
-	public static void main(String args[]){
-	//to get random number between 0 or 1
-	int randomCheck = (int)Math.floor(Math.random()*10%2);
-	if(randomCheck == 1)
-	{
-		System.out.println("you won $1");
-	}
-	else
-	{
-		System.out.println("you loose $1");
+public class GamblingSimulation {
+	// declare constant variable
+	final int STAKE_AMOUNT = 100;
+	final int BET_AMOUNT = 1;
+	final int WINING_CASH = 150;
+	final int LOOSING_CASH = 50;
+
+	int result = STAKE_AMOUNT;
+
+	// Gambling won or lost on 50% of stake
+	public void checkWinLoose() {
+		while (result != WINING_CASH && result != LOOSING_CASH) {
+
+			// to get random number between 0 or 1.
+			int bettingResult = (int) (Math.floor(Math.random() * 10) % 2);
+			if (bettingResult == 1) {
+				result = result + BET_AMOUNT;
+			} else {
+				result = result - BET_AMOUNT;
+			}
+
+		}
+
+		if (result == WINING_CASH) {
+			System.out.println("you won the game");
+			System.out.println("stake: " + result);
+		} else {
+			System.out.println("you loose the game");
+			System.out.println("stake: " + result);
+		}
+
 	}
 
-}
-	
+	public static void main(String args[]) {
+
+		GamblingSimulation gs = new GamblingSimulation();
+		gs.checkWinLoose();
+	}
+
 }
