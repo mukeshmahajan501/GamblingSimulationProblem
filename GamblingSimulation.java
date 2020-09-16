083cde219fcdd424903cd0d7d0e9afe989ac0195
mukesh <mukeshmahajan501@gmail.com>
2020-09-16 10:03:52 +0530

    [Mukesh]Modified:maintain naming convention and remove unwanted space

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -6,25 +6,21 @@ public class GamblingSimulation {
 	final int LOOSING_CASH = 50;
 	final int NO_OF_DAYS = 20;
 	final int STAKE_PERCENTAGE = 50;
+	//declared instance variable
 	int result;
 	int dayResult = 0;
 
 	public void getTotalAmountWinOrLost() {
-
 		for (int day = 1; day <= NO_OF_DAYS; day++) {
 			result = STAKE;
 			while ((result != WINING_CASH) && (result != LOOSING_CASH)) {
 				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
-
 				if (randomCheck == 1) {
 					result = result + BET;
-
 				} else {
 					result = result - BET;
 				}
-
 			}
-
 			if (result == WINING_CASH) {
 				dayResult = dayResult + 1;
 				System.out.println("total Amount won in day:" + day + ": " + result);
@@ -33,28 +29,22 @@ public class GamblingSimulation {
 				dayResult = dayResult - 1;
 				System.out.println("total amount lost in days:" + day + ": " + +result);
 			}
-
 		}
 		int totalAmount = dayResult * STAKE_PERCENTAGE;
-
 		if (totalAmount == 0) {
 			System.out.println("---------------------------------------");
 			System.out.println("No lost No win: "+totalAmount);
 		} else if (totalAmount > 0) {
 			System.out.println("---------------------------------------");
-
 			System.out.println("you won total amount: " + totalAmount);
 		} else {
 			System.out.println("---------------------------------------");
-
 			System.out.println("you lost total amount: " + Math.abs(totalAmount));
 		}
-
 	}
 
 	public static void main(String[] args) {
-		GamblingSimulation obj = new GamblingSimulation();
-		obj.getTotalAmountWinOrLost();
+		GamblingSimulation gsObject = new GamblingSimulation();
+		gsObject.getTotalAmountWinOrLost();
 	}
-
 }
