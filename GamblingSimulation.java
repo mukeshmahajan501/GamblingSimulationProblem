c940a2887546f38f63d1014eec326db56902584a
mukesh <mukeshmahajan501@gmail.com>
2020-09-16 10:11:00 +0530

    [Mukesh]Modified:maintain naming convention and remove unwanted space

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,35 +1,29 @@
 public class GamblingSimulation {
-	//declare constant variable
+	// declare constant variable
 	final int STAKE = 100;
 	final int BET = 1;
 	final int WINING_CASH = 150;
 	final int LOOSING_CASH = 50;
 	final int NO_OF_DAYS = 30;
-
+	// declared array
 	int winArr[] = new int[NO_OF_DAYS];
 	int lostArr[] = new int[NO_OF_DAYS];
 
 	public void checkWinLoose() {
-
 		for (int day = 0; day < NO_OF_DAYS; day++) {
 			int winCount = 0;
 			int lostCount = 0;
 			int result = STAKE;
 			while ((result != WINING_CASH) && (result != LOOSING_CASH)) {
 				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
-
 				if (randomCheck == 1) {
 					result = result + BET;
 					winCount++;
-
 				} else {
 					result = result - BET;
 					lostCount++;
-
 				}
-
 			}
-
 			if (result == WINING_CASH) {
 				System.out.println("you won the game");
 				winArr[day] = winCount;
@@ -39,11 +33,8 @@ public class GamblingSimulation {
 				lostArr[day] = lostCount;
 				System.out.println("total Amount lost: " + lostCount + " in day: " + (day + 1));
 			}
-
 		}
-
 		int maxWin = 0;
-
 		int luckyDay = 0;
 		for (int day = 0; day < NO_OF_DAYS; day++) {
 			if (winArr[day] > maxWin && winArr[day] != 0) {
@@ -51,11 +42,8 @@ public class GamblingSimulation {
 				luckyDay = day;
 			}
 		}
-
 		System.out.println("your lukkiest day:" + (luckyDay + 1));
-
 		int maxLost = 0;
-
 		int unLuckyDay = 0;
 		for (int day = 0; day < NO_OF_DAYS; day++) {
 			if (lostArr[day] > maxLost && lostArr[day] != 0) {
@@ -63,15 +51,12 @@ public class GamblingSimulation {
 				unLuckyDay = day;
 			}
 		}
-
 		System.out.println("your unlukkiest day:" + (unLuckyDay + 1));
-
 	}
 
 	public static void main(String[] args) {
-		GamblingSimulation obj = new GamblingSimulation();
-		obj.checkWinLoose();
+		GamblingSimulation gsObject = new GamblingSimulation();
+		gsObject.checkWinLoose();
 
 	}
-
 }
